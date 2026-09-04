a9a6974734c547744d87953224b1f9dcd6909932
hau <dauphaihau@gmail.com>
2026-09-04 19:00:52 +0700

    chore(config): move private fn to bottom

--- apps/storefront/nuxt.config.ts
+++ apps/storefront/nuxt.config.ts
@@ -15,26 +15,6 @@ const debugEntry = require.resolve('debug/src/index.js')
 const assetHost = process.env.ASSET_HOST || ''
 const awsHostBucketAlias = assetHost.replace(/\/+$/, '')
 
-function manualChunks(id: string): string | undefined {
-  if (id.includes('/node_modules/.pnpm/@vue+') || id.includes('/node_modules/.pnpm/vue@') || id.includes('/node_modules/.pnpm/vue-router@')) {
-    return 'vue'
-  }
-
-  if (id.includes('/node_modules/.pnpm/@nuxt+ui@') || id.includes('/node_modules/.pnpm/@headlessui+vue@') || id.includes('/node_modules/.pnpm/@popperjs+core@') || id.includes('/node_modules/.pnpm/@tanstack+virtual-core@') || id.includes('/node_modules/.pnpm/@tanstack+vue-virtual@')) {
-    return 'ui'
-  }
-
-  if (id.includes('/node_modules/.pnpm/@nuxtjs+i18n@') || id.includes('/node_modules/.pnpm/vue-i18n@') || id.includes('/node_modules/.pnpm/@intlify+')) {
-    return 'i18n'
-  }
-
-  if (id.includes('/node_modules/.pnpm/@tanstack+query-core@') || id.includes('/node_modules/.pnpm/@tanstack+vue-query@') || id.includes('/node_modules/.pnpm/@hebilicious+vue-query-nuxt@')) {
-    return 'query'
-  }
-
-  return undefined
-}
-
 export default defineNuxtConfig({
   app: {
     head: {
@@ -224,3 +204,23 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-08-21',
 })
+
+function manualChunks(id: string): string | undefined {
+  if (id.includes('/node_modules/.pnpm/@vue+') || id.includes('/node_modules/.pnpm/vue@') || id.includes('/node_modules/.pnpm/vue-router@')) {
+    return 'vue'
+  }
+
+  if (id.includes('/node_modules/.pnpm/@nuxt+ui@') || id.includes('/node_modules/.pnpm/@headlessui+vue@') || id.includes('/node_modules/.pnpm/@popperjs+core@') || id.includes('/node_modules/.pnpm/@tanstack+virtual-core@') || id.includes('/node_modules/.pnpm/@tanstack+vue-virtual@')) {
+    return 'ui'
+  }
+
+  if (id.includes('/node_modules/.pnpm/@nuxtjs+i18n@') || id.includes('/node_modules/.pnpm/vue-i18n@') || id.includes('/node_modules/.pnpm/@intlify+')) {
+    return 'i18n'
+  }
+
+  if (id.includes('/node_modules/.pnpm/@tanstack+query-core@') || id.includes('/node_modules/.pnpm/@tanstack+vue-query@') || id.includes('/node_modules/.pnpm/@hebilicious+vue-query-nuxt@')) {
+    return 'query'
+  }
+
+  return undefined
+}
